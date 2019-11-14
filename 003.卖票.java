7134d7648645db740a2f13a94f672c47fed45df4
afra666 <48674984+afra666@users.noreply.github.com>
2019-11-14 12:33:10 +0800

    Update 003.卖票.java

--- 003.卖票.java
+++ 003.卖票.java
@@ -1,21 +1,22 @@
 class MyRunnable implements Runnable{
     int remainTickets=5;
     @Override
-    public void run() {
+    public void run() {//3.运行状态
         while (remainTickets>0){
             remainTickets--;
-            System.out.println(Thread.currentThread().getName()+"卖出一张票，剩余票数："+remainTickets);
+            System.out.println(Thread.currentThread().getName()+"卖了一张票，剩余票数："+remainTickets);
         }
-    }
+    }//4.终止状态
+//    如果有sleep，将进入阻塞状态，让出线程
 }
 
-public class App {
+public class App{
     public static void main(String[] args) {
         MyRunnable mr=new MyRunnable();
-        Thread mr1=new Thread(mr);
+        Thread mr1=new Thread(mr);//1.创建线程
         Thread mr2=new Thread(mr);
         Thread mr3=new Thread(mr);
-        mr1.start();
+        mr1.start();//2.就绪
         mr2.start();
         mr3.start();
     }
